fc4174dd3d7f156241191f2977c552d93a4b7a99
Matheus Couto <37025583+matheusccouto@users.noreply.github.com>
2023-09-25 00:06:15 +0000

    Add backlog

---
+++ backlog.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+S3_URI="s3://cartola-main/atletas/mercado"
+GCS_URI="gs://cartola/atletas/mercado/atletas"
+KEY="atletas"
+
+aws s3 sync $S3_URI ./backlog --exclude "*" --include "*.json"
+
+# Iterate through all the JSON files that match the pattern
+for file in backlog/*.json; do
+
+    # Get the base filename without the directory
+    base=$(basename "$file")
+
+    # Remove extension.
+    base="${base%.json}" 
+
+    # Extract the SEASON and ROUND from the filename
+    SEASON="${base%-*}"  # Extracts portion before '-'
+    ROUND="${base#*-}" # Extracts portion after '-'
+
+    echo Modifying $SEASON-$ROUND
+
+    ROUND="${ROUND#"${ROUND%%[1-9]*}"}"  # Remove leading zero
+
+    # Use jq to update the JSON and overwrite the original file
+    jq .atletas $file | \  # FIXME
+    jq --arg S $SEASON --arg R $ROUND 'map(. + {"_temporada": $S, "_rodada": $R})' | \
+    jq -c '.[]' > "${file}.tmp"
+
+    # mv "${file}.tmp" "$file"
+
+done
+
+
+# # Overwrite the AWS files with the existing from GCP
+# gsutil -m cp -r  $GCS_URI/*.json backlog
+
+
+# # Iterate through all the JSON files that match the pattern
+# for file in backlog/*.json; do
+
+#     # Get the base filename without the directory
+#     base=$(basename "$file")
+
+#     gcloud storage cp $file $GCS_URI/$base.json
